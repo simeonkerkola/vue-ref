3a7e4091906fd833a3a0c343d2249813499637da
sssmi <simeon.kerkola@gmail.com>
2018-08-06 15:30:36 +0300

    Add nuxt

---
+++ destination-cli/src/App.vue
@@ -0,0 +1,90 @@
+<template>
+  <div id="app">
+    <h1>Your Upcoming Destinations</h1>
+    <div class="location-contain">
+      <div class="locations">
+        <div v-for="location in locations">
+          <place 
+            :name="location.name" 
+            :img="location.img" 
+            :description="location.desc" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Place from './components/Place.vue';
+export default {
+  name: 'App',
+  components: {
+    Place
+  },
+  data() {
+    return {
+      locations: [
+        {
+          name: 'moscow',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
+          desc: `Moscow is the capital and most populous city of Russia, with 
+          13.2 million residents within the city limits and 17.8 million within 
+          the urban area. Moscow has the status of a Russian federal city.`
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
+          desc: `Paris is the capital and most populous city of France. By 
+          the 17th century, Paris was one of Europe's major centres of finance, 
+          commerce, fashion, science, and the arts, and it retains that position still today.`
+        },
+        {
+          name: 'rome',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology 
+          dates the founding of Rome at around 753 BC, the site has been inhabited 
+          for much longer, making it one of the oldest continuously occupied sites
+           in Europe.`
+        },
+        {
+          name: 'paris',
+          img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
+          desc: `Paris is the capital and most populous city of France. By the 
+          17th century, Paris was one of Europe's major centres of finance, commerce, 
+          fashion, science, and the arts, and it retains that position still today.`
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  width: 100vw;
+  height: 100vh;
+  font-family: 'NTR', sans-serif;
+  background: #eee;
+  font-size: 16px;
+}
+
+h1 {
+  text-align: center;
+}
+
+.location-contain {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  margin: 10px;
+  align-self: center;
+}
+
+h2 {
+  text-transform: capitalize;
+  margin: 0;
+  text-align: center;
+}
+</style>
---
+++ destination-cli/src/components/Place.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="place">
+    <img 
+      :src="img" 
+      width="235" 
+      height="300">
+    <h2>{{ name }}</h2>
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    img: { type: String, required: true }
+  }
+};
+</script>
+
+<style scoped>
+.place {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 320px;
+  justify-content: center;
+  background: white;
+  border: 1px solid #ddd;
+  padding: 20px 20px;
+  margin: 10px;
+}
+</style>
