081174f3353eb1f9a19c659b871ae023856305a6
sssmi <simeon.kerkola@gmail.com>
2018-08-06 09:56:19 +0300

    Refactor a bit

--- exc-3/script.js
+++ exc-3/script.js
@@ -1,5 +1,9 @@
 Vue.component('place', {
-  props: ['name', 'description', 'img'],
+  props: {
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    img: { type: String, required: true }
+  },
   template: '#location-template',
 })
 
@@ -11,22 +15,31 @@ new Vue({
         {
           name: 'moscow',
           img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/moscow.svg',
-          desc: `Moscow is the capital and most populous city of Russia, with 13.2 million residents within the city limits and 17.8 million within the urban area. Moscow has the status of a Russian federal city.`
+          desc: `Moscow is the capital and most populous city of Russia, with 
+          13.2 million residents within the city limits and 17.8 million within 
+          the urban area. Moscow has the status of a Russian federal city.`
         },
         {
           name: 'paris',
           img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris1.svg',
-          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+          desc: `Paris is the capital and most populous city of France. By 
+          the 17th century, Paris was one of Europe's major centres of finance, 
+          commerce, fashion, science, and the arts, and it retains that position still today.`
         },
         {
           name: 'rome',
           img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/rome.svg',
-          desc: `Rome's history spans more than 2,500 years. While Roman mythology dates the founding of Rome at around 753 BC, the site has been inhabited for much longer, making it one of the oldest continuously occupied sites in Europe.`
+          desc: `Rome's history spans more than 2,500 years. While Roman mythology 
+          dates the founding of Rome at around 753 BC, the site has been inhabited 
+          for much longer, making it one of the oldest continuously occupied sites
+           in Europe.`
         },
         {
           name: 'paris',
           img: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/paris2.svg',
-          desc: `Paris is the capital and most populous city of France. By the 17th century, Paris was one of Europe's major centres of finance, commerce, fashion, science, and the arts, and it retains that position still today.`
+          desc: `Paris is the capital and most populous city of France. By the 
+          17th century, Paris was one of Europe's major centres of finance, commerce, 
+          fashion, science, and the arts, and it retains that position still today.`
         },
       ]
     };
