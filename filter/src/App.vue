d529f58bccc5b4aa0d9e815f65bbc0937b46b106
sssmi <simeon.kerkola@gmail.com>
2018-08-08 09:40:18 +0300

    Complete filters

---
+++ filter/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <small>Your place here: </small>
+    <input v-model="num">
+    <div class="won">
+      <h2>Congratulations!</h2>
+      <p v-if="num"> You won <span>{{ num | place }}</span> place!</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  filters: {
+    place: function(value) {
+      if (value == 1) return `${value}st`;
+      else if (value == 2) return `${value}nd`;
+      else if (value == 3) return `${value}rd`;
+      return `${value}th`;
+    }
+  },
+  data() {
+    return {
+      num: 2
+    };
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: sans-serif;
+}
+
+#app {
+  text-align: center;
+  max-width: 420px;
+  margin: 50px auto;
+  display: table;
+  padding: 10px 40px;
+  border: 1px solid black;
+}
+
+h2 {
+  border-bottom: 1px solid #ed9909;
+}
+
+span {
+  font-size: 40px;
+}
+
+.won {
+  background: #ffc968;
+  padding: 10px 20px;
+  border-radius: 3px;
+  margin: 10px;
+}
+</style>
